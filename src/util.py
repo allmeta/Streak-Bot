f95750a96462c8fcf138caed78774f7c357e2260
allmeta <thomasdasd@gmail.com>
2020-10-03 01:35:33 +0200

    fix spacing rip (#3)

--- src/util.py
+++ src/util.py
@@ -70,10 +70,10 @@ async def user_update_nickname(conn, bot, icons, member, serverid):
         user = bot.get_user(member.id)
         # checks if they has a nickname, and tries to match on icons
         # same as in reset_nickname
-        s=f'^\d+({"|".join(icons)})'
+        s=f'^\d+({"|".join(icons)})\ '
         if (match:=re.compile(s).match(nick)):
             nick = ''.join(nick.split(match.group(1))[1:])
-        nick = f'{current_streak}{current_icon}{nick}'
+        nick = f'{current_streak}{current_icon} {nick}'
         try:
             await member.edit(nick=nick)
         except discord.errors.Forbidden:
